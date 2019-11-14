65384a41fd8da24362a97def99b3d3467bf02fa8
bobylapastek <57773686+bobylapastek@users.noreply.github.com>
2019-11-14 22:16:54 +0100

    Create command.php

---
+++ command.php
@@ -0,0 +1,21 @@
+<?php include("includes/la-config.php");?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <?php include("includes/header.php");?>
+  </head>
+  <body>
+
+  <?php include("includes/thebody.php");?>
+
+  <div>
+    <h1>THIS IS A WEBSITE</h1>
+    <p>
+      I always dreamed about creating a website. Not any kind of website : a futurist one. You know, the one you just create to make some test for your application ? That's exactly what i'm doing write now : that's definitely not a futurist website, i'm truely sorry about my stupid dreams. Hope you enjoyed reading.
+    </p>
+  </div>
+
+  <?php include("includes/footer.php");?>
+
+  </body>
+</html>
